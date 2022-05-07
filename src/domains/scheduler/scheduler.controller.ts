08fb6ae9bca15995e880cf3a1d8d277743a154a8
inho <inho@gomicorp.com>
2022-05-07 21:20:47 +0900

    [feat] 오류 수정

--- src/domains/scheduler/scheduler.controller.ts
+++ src/domains/scheduler/scheduler.controller.ts
@@ -8,7 +8,7 @@ export class SchedulerController {
 
   @Get('/run')
   async run(): Promise<string> {
-    await this.schedulerService.handleCron();
+    await this.schedulerService.scrap();
     return '음원순위 스크래핑이 수동으로 동작하였습니다.';
   }
 }
