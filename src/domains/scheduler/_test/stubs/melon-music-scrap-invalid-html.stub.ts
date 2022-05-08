90fe94adaa934670a50f50ef0d851e310e0f362c
inho <inho@gomicorp.com>
2022-05-08 14:30:35 +0900

    [feat] 변경된 scrap 로직으로 테스트 코드에 추가

---
+++ src/domains/scheduler/_test/stubs/melon-music-scrap-invalid-html.stub.ts
@@ -0,0 +1,49 @@
+export const melonMusicScrapInvalidHtmlStub = `
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+\t<meta charset="utf-8">
+\t<meta http-equiv="X-UA-Compatible" content="IE=edge">
+\t<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+\t<meta name="format-detection" content="telephone=no">
+\t<meta name="description" content="좋아하는 음악은 물론, 좋아할 음악까지 들려주는 취향 저격 뮤직 서비스">
+\t<meta name="apple-itunes-app" content="app-id=1338631589">
+\t<meta property="og:type" content="website">
+\t<meta property="og:image" content="https://music-phinf.pstatic.net/20201019_118/16031004163365Jexk_JPEG/about.jpg">
+\t<meta property="og:title" content="NAVER VIBE(바이브)">
+\t<meta property="og:description" content="#나만을 위한 음악이 듣고 싶을 땐 #내돈내듣 VIBE.">
+\t<meta property="og:url" content="https://vibe.naver.com">
+\t<meta name="twitter:card" content="summary_large_image">
+\t<meta name="twitter:image" content="https://music-phinf.pstatic.net/20201019_118/16031004163365Jexk_JPEG/about.jpg">
+\t<meta name="twitter:title" content="NAVER VIBE(바이브)">
+\t<meta name="twitter:site" content="NAVER VIBE(바이브)">
+\t<meta name="twitter:creator" content="VIBE">
+\t<meta name="twitter:description" content="#나만을 위한 음악이 듣고 싶을 땐 #내돈내듣 VIBE.">
+\t<meta name="facebook-domain-verification" content="bd4b0v5vmt0svjpl26q5rtwbved1mp">
+\t<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+\t<link rel="icon" href="/favicon.ico" type="image/x-icon">
+\t<link rel="apple-touch-icon" href="/images/icon/icon_120x120.png">
+\t<link rel="apple-touch-icon" sizes="120x120" href="/images/icon/icon_120x120.png">
+\t<link rel="apple-touch-icon" sizes="152x152" href="/images/icon/icon_152x152.png">
+\t<link rel="apple-touch-icon" sizes="167x167" href="/images/icon/icon_167x167.png">
+\t<link rel="apple-touch-icon" sizes="180x180" href="/images/icon/icon_180x180.png">
+\t<title>VIBE (바이브)</title>
+\t<link href="/20220505141527_resources/css/common.c740f88f.css" rel="prefetch">
+\t<link href="/20220505141527_resources/js/common.673e9092.js" rel="prefetch">
+\t<link href="/20220505141527_resources/css/app.fd71f316.css" rel="preload" as="style">
+\t<link href="/20220505141527_resources/css/chunk-vendors.ae881fe1.css" rel="preload" as="style">
+\t<link href="/20220505141527_resources/js/app.52bf0d9a.js" rel="preload" as="script">
+\t<link href="/20220505141527_resources/js/chunk-vendors.e7e298d1.js" rel="preload" as="script">
+\t<link href="/20220505141527_resources/css/chunk-vendors.ae881fe1.css" rel="stylesheet">
+\t<link href="/20220505141527_resources/css/app.fd71f316.css" rel="stylesheet">
+</head>
+
+<body><noscript><strong>자바스크립트가 비활성화 되어있어 화면을 표시할 수 없습니다. 브라우저 설정에서 자바스크립트를 활성화 후 다시 시도해 주세요.</strong></noscript>
+\t<div id="app" class="app"></div>
+\t<script src="/20220505141527_resources/js/chunk-vendors.e7e298d1.js"></script>
+\t<script src="/20220505141527_resources/js/app.52bf0d9a.js"></script>
+</body>
+
+</html>
+`;
--- src/domains/scheduler/logics/scrap-music.spec.ts
+++ src/domains/scheduler/logics/scrap-music.spec.ts
@@ -1,7 +1,9 @@
+import { errorMessageDictionary } from '../../../dictionary/error-message.dictionary';
 import { vendorInformationDictionary } from '../../../dictionary/vendor-information.dictionary';
 import { VendorEnum } from '../../../enums/vendor.enum';
 import { axiosWrapper } from '../../../utils/axios.wrapper';
 import { melonMusicScrapHtmlStub } from '../_test/stubs/melon-music-scrap-html.stub';
+import { melonMusicScrapInvalidHtmlStub } from '../_test/stubs/melon-music-scrap-invalid-html.stub';
 import { MusicSummaryInterface } from '../interfaces/music-summary.interface';
 import { extractAlbumId, scrapMusic } from './scrap-music';
 
@@ -21,6 +23,46 @@ describe('scrap-music.spec.ts', () => {
 
     it('앨범 ID가 정상인지 체크', () => {
       expect(output).toBe('050505');
+    });
+  });
+
+  describe('멜론 음악 스크랩핑 데이터 axios가 정상적이지 않을 때 추출', () => {
+    let output: Error;
+    beforeEach(async () => {
+      jest.spyOn(axiosWrapper, 'get').mockResolvedValue(null);
+
+      try {
+        await scrapMusic(vendorInformationDictionary[VendorEnum.MELON].music);
+      } catch (error) {
+        output = error;
+      }
+    });
+
+    it('데이터 내용 정상인지 체크', () => {
+      expect(output.message).toBe(
+        errorMessageDictionary.MUSIC_SCRAPPING_AXIOS_INVALID,
+      );
+    });
+  });
+
+  describe('멜론 음악 스크랩핑 데이터 axios 정상이지만 추출할 타겟이 존재하지 않을 때 추출', () => {
+    let output: Error;
+    beforeEach(async () => {
+      jest
+        .spyOn(axiosWrapper, 'get')
+        .mockResolvedValue(melonMusicScrapInvalidHtmlStub);
+
+      try {
+        await scrapMusic(vendorInformationDictionary[VendorEnum.MELON].music);
+      } catch (error) {
+        output = error;
+      }
+    });
+
+    it('데이터 내용 정상인지 체크', () => {
+      expect(output.message).toBe(
+        errorMessageDictionary.MUSIC_SCRAPPING_PAGE_INVALID,
+      );
     });
   });
 
